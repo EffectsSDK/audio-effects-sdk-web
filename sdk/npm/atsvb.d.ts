565f6cb4ab1a15c19b6b06e66cc2db3fa41d5d54
Eugeny Dementev <eugeny.dementev@ya.ru>
2025-04-24 15:10:50 +0800

    extend error types export

--- sdk/npm/atsvb.d.ts
+++ sdk/npm/atsvb.d.ts
@@ -1,3 +1,3 @@
 export { atsvb } from "./types/atsvb";
-export { ErrorType } from "./types/utils/errorBus";
+export { ErrorObject, ErrorType, ErrorCode, ErrorEmitter } from "./types/utils/errorBus";
 export { ModelType, DefaultConfig, Config } from "./types/settings";
